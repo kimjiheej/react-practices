cb25b3f1d24389dbd2ccb4ace186796b066e5b2c
kimjiheej <glp0509@naver.com>
2024-07-17 09:46:32 +0900

    task box 에 적용

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {_Card, Card_Title} from   './assets/scss/Card.scss';
+import { _Card, Card_Title, Card_Title_Open } from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 
 function Card({ title, description, tasks, isOpen }) {
   return (
-    <div className={`_Card ${isOpen ? 'Card_Title_Open' : ''}`}>
+    <div className={`${_Card} ${isOpen ? Card_Title_Open : ''}`}>
       <div className={Card_Title}>{title}</div>
       <div className="Card_Details">
         {description}
