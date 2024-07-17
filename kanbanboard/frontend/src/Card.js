cf66129d262431b626ec4b3d72d66c73981b056f
kimjiheej <glp0509@naver.com>
2024-07-17 09:52:22 +0900

    체크박스 수정

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { _Card, Card_Title, Card_Title_Open } from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 
-function Card({ title, description, tasks, isOpen }) {
+function Card({ title, description, tasks }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCard = () => {
+    setIsOpen(!isOpen); // isOpen 상태를 반전시킴
+  };
+
   return (
     <div className={`${_Card} ${isOpen ? Card_Title_Open : ''}`}>
-      <div className={Card_Title}>{title}</div>
-      <div className="Card_Details">
-        {description}
-        <TaskList tasks={tasks} />
-      </div>
+      <div className={Card_Title} onClick={toggleCard}>{title}</div>
+      {isOpen && (
+        <div className="Card_Details">
+          {description}
+          <TaskList tasks={tasks} />
+        </div>
+      )}
     </div>
   );
 }
--- kanbanboard/frontend/src/Task.js
+++ kanbanboard/frontend/src/Task.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import {_Task, Task_Remove} from './assets/scss/Task.scss';
+import React, { useState } from 'react';
+import { _Task, Task_Remove } from './assets/scss/Task.scss';
 
 function Task({ task, onRemove }) {
+  const [isChecked, setIsChecked] = useState(task.done);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked); // isChecked 상태를 반전시킴
+    // 여기에서 task.done 상태를 부모 컴포넌트로 업데이트할 수도 있습니다.
+    // onToggleDone(task.no, !isChecked); // 예를 들어 부모 컴포넌트로 상태 업데이트 함수를 호출할 수 있습니다.
+  };
+
   return (
-    <li className="{_Task}">
-      <input type="checkbox" checked={task.done} readOnly />
+    <li className={_Task}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
       {task.name}
       <a href="#" className={Task_Remove} onClick={() => onRemove(task.no)}></a>
     </li>
