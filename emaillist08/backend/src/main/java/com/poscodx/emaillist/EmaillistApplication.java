671e45649acb39b3c550ddbc24864e1026979a92
kimjiheej <glp0509@naver.com>
2024-07-18 11:41:08 +0900

    api 사용

--- emaillist08/backend/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
@@ -3,10 +3,14 @@ package com.poscodx.emaillist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.poscodx.emaillist.vo.EmaillistVo;
+
 @SpringBootApplication
 public class EmaillistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmaillistApplication.class, args);
+       
     }
 }
+
