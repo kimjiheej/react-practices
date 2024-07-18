7db7ba5b754cf227fef003bd18c4874a7a00ea8a
kimjiheej <glp0509@naver.com>
2024-07-18 17:48:11 +0900

    github 수정

--- emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -3,7 +3,9 @@ package com.poscodx.emaillist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +31,6 @@ public class ApiController {
 					.status(HttpStatus.OK)
 					.body(JsonResult.success(emaillistRepository.findAll(keyword)));
 	}
-	
 	
 	@PostMapping("/api")
 	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {
