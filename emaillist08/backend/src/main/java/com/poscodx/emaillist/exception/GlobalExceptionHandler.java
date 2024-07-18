0a05cc1ac88c02ed4ecd7cecef5a318d21df01f2
kimjiheej <glp0509@naver.com>
2024-07-18 11:11:45 +0900

    Handler 수정 완료

--- emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
@@ -7,12 +7,9 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poscodx.emaillist.dto.JsonResult;
@@ -22,26 +19,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
-			
 	@ExceptionHandler(Exception.class)
-	public void handler(
-		HttpServletRequest request,
-		HttpServletResponse response,
-		Exception e
+	public void handler(HttpServletRequest request, HttpServletResponse response, Exception e
 	) throws Exception {
-		//1. 로깅(logging)
+		
+		// logging
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
 		
-		//2. 요청구분
-		// json 요청: request header에 application/json (o)
-		// html 요청: request header에 application/json (x)
+		// 500 for json request
 		String accept = request.getHeader("accept");
-		
 		if(accept.matches(".*application/json.*")) {
-			//3. json 응답
 			JsonResult jsonResult = JsonResult.fail(errors.toString());
 			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
 			
@@ -50,20 +39,20 @@ public class GlobalExceptionHandler {
 			OutputStream os = response.getOutputStream();
 			os.write(jsonString.getBytes("utf-8"));
 			os.close();
-			return; 
-		} else {
-			// for html requset 
-			if(e instanceof NoHandlerFoundException) {
-				
-				request
-				.getRequestDispatcher("/error/404")
-				.forward(request, response);
-			} else {
-				request.setAttribute("error", errors.toString());
-				request
-					.getRequestDispatcher("/error/500")
-					.forward(request, response);
-			}
+			
+			return;
 		}
+
+		// 404 for html request
+		if(e instanceof NoHandlerFoundException) {
+			request.getRequestDispatcher("/error/404").forward(request, response);
+			return;
+		}
+		
+		// 500 for html request
+		request.setAttribute("error", errors.toString());
+		request
+			.getRequestDispatcher("/error/500")
+			.forward(request, response);
 	}
 }
