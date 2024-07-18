f457363efb6bdfe7638f5d7548bc8da51ae5f287
kimjiheej <glp0509@naver.com>
2024-07-18 17:04:58 +0900

    fixing react

---
+++ 04.Routing/src/01/component/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <div>
+            <h1>Main</h1>
+            <p>
+                <a href="#/"></a>
+            </p>
+        </div>
+    );
+}
